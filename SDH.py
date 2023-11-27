f2d537cbf05ed1e5e43c098e97e2f13ec63089cc
Joshua731 <88397658+Joshua731@users.noreply.github.com>
2023-11-27 08:44:49 -0300

    update sign in interface

--- SDH.py
+++ SDH.py
@@ -1,4 +1,8 @@
 from app import app
 
+# database: bancobrasil
+# ip e porta: 201.48.100.251;1434
+# usuario: capua
+# senha: Capua123#
 
 app.run(host='0.0.0.0', debug=True, port=5001)
--- app/templates/formularios/cadastro_banco_parte_1.py
+++ app/templates/formularios/cadastro_banco_parte_1.py
@@ -1,5 +1,8 @@
+import datetime
+
 import dash
 import pandas as pd
+import pyodbc
 import requests
 import sqlalchemy
 from dash.exceptions import PreventUpdate
@@ -40,9 +43,10 @@ estados = [
     {'label': 'Tocantins', 'value': 'TO'}
 ]
 
-df_banco = None
-engine = None
+variaveis_globais = {}
 
+df_variaveis = pd.DataFrame({'quantidade_de_inversores': [4]})
+df_variaveis.to_excel(r'app\templates\formularios\temp.xlsx', index=False)
 df = pd.read_csv(r'app\files\saida_atualizado.csv')
 
 cad_banco_1 = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR], server=app, url_base_pathname='/cadastro-1/')
@@ -92,7 +96,7 @@ cad_banco_1.layout = dbc.Container([
                         dbc.Col(sm=3),
                         dbc.Col([
                             dbc.Card([
-                                dbc.CardHeader("Integração da fonte de dados"),
+                                dbc.CardHeader("Integração da fonte de dados", class_name='card-header-banco'),
                                 dbc.CardBody([
                                     dbc.Row([
                                         dcc.Dropdown(
@@ -102,13 +106,11 @@ cad_banco_1.layout = dbc.Container([
                                                 {"label": 'MySQL', 'value': 'mysql'},
                                                 {'label': 'SQL Server', 'value': 'sql-server'},
                                             ],
-                                            style={'background-color': 'rgb(7,54,66)',
-                                                   'placeholder': {'color': 'white'}},
                                         )
                                     ]),
                                     dbc.Row(id='conteudo-origem'),
                                 ])
-                            ], color='dark')
+                            ], color='dark', id='card-banco')
                         ], sm=6),
                         dbc.Col(sm=3),
                     ])
@@ -123,13 +125,21 @@ cad_banco_1.layout = dbc.Container([
                                         dbc.Tab(tab_id='tab-invs', label='Inversores', children=[
                                             dbc.Row([
                                                 dbc.Col([
-                                                    dbc.Checkbox(
-                                                        id='check-invs',
-                                                        label='Possui Inversores? (caso não possuir, apenas ignore)',
-                                                        value=False,
-                                                    )
+                                                    dbc.Row([
+                                                        dbc.Checkbox(
+                                                            id='check-invs',
+                                                            label='Possui Inversores? (caso não possuir, apenas ignore)',
+                                                            value=False,
+                                                        )
+                                                    ]),
+                                                    dbc.Row([
+                                                        dbc.Col([
+                                                            dbc.Button('Próximo', color='dark', id='red-cm')
+                                                        ], sm=2)
+                                                    ])
                                                 ], sm=12)
-                                            ])
+                                            ]),
+                                            dbc.Row(id='row-invs'),
                                         ]),
                                         dbc.Tab(tab_id='tab-cm', label='Central Meteorológica', children=[
                                             dbc.Row([
@@ -140,7 +150,8 @@ cad_banco_1.layout = dbc.Container([
                                                         value=False,
                                                     )
                                                 ], sm=12)
-                                            ])
+                                            ]),
+                                            dbc.Row(id='row-cm')
                                         ]),
                                         dbc.Tab(tab_id='tab-idgt', label='IDGT', children=[
                                             dbc.Row([
@@ -151,7 +162,8 @@ cad_banco_1.layout = dbc.Container([
                                                         value=False,
                                                     )
                                                 ], sm=12)
-                                            ])
+                                            ]),
+                                            dbc.Row(id='row-idgt')
                                         ]),
                                         dbc.Tab(tab_id='tab-qgbt', label='QGBT', children=[
                                             dbc.Row([
@@ -163,32 +175,39 @@ cad_banco_1.layout = dbc.Container([
                                                     )
                                                 ], sm=12)
                                             ]),
+                                            dbc.Row(id='row-qgbt')
                                         ]),
                                         dbc.Tab(tab_id='tab-irrad-pot', label='Irradiância x Potência', children=[
                                             dbc.Row([
                                                 dbc.Col([
-                                                    dbc.Card([
-                                                        dbc.CardHeader('Irradiância'),
-                                                        dbc.CardBody([
-                                                            dbc.Checkbox(
-                                                                id='check-irrad',
-                                                                label='Possui Irradiância? (caso não possuir, apenas ignore)',
-                                                                value=False,
-                                                            )
-                                                        ])
-                                                    ], color='dark')
+                                                    dbc.Row([
+                                                        dbc.Card([
+                                                            dbc.CardHeader('Irradiância'),
+                                                            dbc.CardBody([
+                                                                dbc.Checkbox(
+                                                                    id='check-irrad',
+                                                                    label='Possui Irradiância? (caso não possuir, apenas ignore)',
+                                                                    value=False,
+                                                                )
+                                                            ])
+                                                        ], color='dark')
+                                                    ]),
+                                                    dbc.Row(id='row-irrad')
                                                 ], sm=6),
                                                 dbc.Col([
-                                                    dbc.Card([
-                                                        dbc.CardHeader('Potência'),
-                                                        dbc.CardBody([
-                                                            dbc.Checkbox(
-                                                                id='check-pot',
-                                                                label='Possui Potência Ativa? (caso não possuir, apenas ignore)',
-                                                                value=False,
-                                                            )
-                                                        ])
-                                                    ], color='dark')
+                                                    dbc.Row([
+                                                        dbc.Card([
+                                                            dbc.CardHeader('Potência'),
+                                                            dbc.CardBody([
+                                                                dbc.Checkbox(
+                                                                    id='check-pot',
+                                                                    label='Possui Potência Ativa? (caso não possuir, apenas ignore)',
+                                                                    value=False,
+                                                                )
+                                                            ])
+                                                        ], color='dark')
+                                                    ]),
+                                                    dbc.Row(id='row-pot')
                                                 ], sm=6),
                                             ])
                                         ]),
@@ -202,6 +221,7 @@ cad_banco_1.layout = dbc.Container([
                                                     ),
                                                 ])
                                             ]),
+                                            dbc.Row(id='row-prod')
                                         ])
                                     ])
                                 ]),
@@ -223,7 +243,6 @@ cad_banco_1.layout = dbc.Container([
                                                 options=[{'label': 'São Paulo', 'value': 'SP'}],
                                                 placeholder='Selecione o(s) estado(s) da(s) usina(s)',
                                                 multi=True,
-                                                style={'background-color': 'rgb(7,54,66)'},
                                             )
                                         ], sm=6),
                                         dbc.Col([
@@ -232,7 +251,6 @@ cad_banco_1.layout = dbc.Container([
                                                 options=[{'label': cidade, 'value': cidade} for cidade in df['name']],
                                                 placeholder='Selecione a(s) cidade(s) da(s) usina(s)',
                                                 multi=True,
-                                                style={'background-color': 'rgb(7,54,66)'},
                                             )
                                         ], sm=6)
                                     ])
@@ -313,12 +331,13 @@ def mostrar_dados_pelo_tipo_de_cadastro(tipo):
                     ])
                 ], sm=3)
             ]),
-            dbc.Row(id='status-banco'),
             dbc.Row([
                 dbc.Col([
-                    dbc.Button('Confirma', id='red-cols', color='dark')
+                    dbc.Button('Próximo', id='red-cols', color='dark')
                 ], sm=2)
             ]),
+            dbc.Row(id='status-banco'),
+
         ], sm=12)
     if not tipo in ['mysql', 'sql-server']:
         return html.P('Estamos trabalhando nessa funcionalidade')
@@ -327,6 +346,8 @@ def mostrar_dados_pelo_tipo_de_cadastro(tipo):
 @cad_banco_1.callback(
     Output('tabs-cad', 'active_tab'),
     Output('status-banco', 'children'),
+    Output('card-banco', 'color'),
+    Output('red-cols', 'color'),
     Input('drop-origem', 'value'),
     Input('input-db', 'value'),
     Input('input-usuario', 'value'),
@@ -336,19 +357,145 @@ def mostrar_dados_pelo_tipo_de_cadastro(tipo):
     Input('red-cols', 'n_clicks'),
 )
 def redireciona_para_colunas(tipo, base, usuario, senha, ip, porta, botao):
-    global df_banco, engine
     if dash.ctx.triggered_id == 'red-cols':
         if tipo == 'sql-server':
-            query = '''
-                SELECT TABLE_NAME
-                FROM INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_TYPE = 'BASE TABLE'
-            '''
             try:
                 engine = create_engine(
                     f'mssql+pyodbc://{usuario}:{senha}@{ip}:{porta}/{base}?driver=ODBC+Driver+17+for+SQL+Server')
-                return 'sel-cols', html.P('OK')
+                df_variaveis[
+                    'engine'] = f'mssql+pyodbc://{usuario}:{senha}@{ip}:{porta}/{base}?driver=ODBC+Driver+17+for+SQL+Server'
+                df_variaveis['tipo'] = tipo
+                df_variaveis['db'] = base
+                df_variaveis['usuario'] = usuario
+                df_variaveis['senha'] = senha
+                df_variaveis['ip'] = ip
+                df_variaveis['porta'] = porta
+                df_variaveis.to_excel(r'app\templates\formularios\temp.xlsx', index=False)
+                query_teste = f'''
+                        SELECT DISTINCT schema_name(schema_id)
+                        FROM sys.tables;
+                        '''
+                pd.read_sql(query_teste, engine)
+                return 'sel-cols', html.P('OK'), 'success', 'success'
             except Exception as e:
                 print(e)
-                return 'cad-db', html.P(e)
+                print(datetime.datetime.now())
+                return 'cad-db', html.P('Ocorreu um erro. Cheque suas informações'), 'danger', 'danger'
     raise PreventUpdate
+
+
+@cad_banco_1.callback(
+    Output('row-invs', 'children'),
+    Input('check-invs', 'value'),
+)
+def mostra_opcoes_dos_inversores(mostrar):
+    if mostrar:
+        return [
+            dbc.Col([
+                dbc.Row([
+                    dcc.Loading([
+                        dbc.Card([
+                            dbc.CardHeader('Inversores'),
+                            dbc.CardBody([
+                                dbc.InputGroup([
+                                    dbc.Col([
+                                        dbc.Input(id='ipt-n-invs', placeholder='Quantidade', type='number')
+                                    ], sm=4),
+                                    dbc.Col([
+                                        dbc.Input(id='ipt-tbl-base', placeholder='Tabela', type='text')
+                                    ], sm=8),
+                                ]),
+                            ])
+                        ], color='dark'),
+                    ])
+                ]),
+                dbc.Row([
+                    dcc.Loading([
+                        dbc.Card([
+                            dbc.CardHeader("Colunas"),
+                            dbc.CardBody(id='cd-bd-cols-invs')
+                        ], color='dark')
+                    ], type='default')
+                ]),
+            ], sm=12),
+        ]
+
+
+@cad_banco_1.callback(
+    Output('cd-bd-cols-invs', 'children'),
+    Input('ipt-tbl-base', 'value'),
+    Input('ipt-n-invs', 'value'),
+)
+def mostra_dropdown_das_colunas_das_tabelas(tabela, n):
+    try:
+        df2 = pd.read_excel(r'app\templates\formularios\temp.xlsx')
+        df2['tabela_dos_inversores'] = tabela
+        df2['quantidade_de_inversores'] = n
+        df2.to_excel(r'app\templates\formularios\temp.xlsx', index=False)
+        query_colunas = f'''
+        select * from {tabela}
+        '''
+        df_colunas = pd.read_sql(query_colunas, create_engine(str(df2['engine'].values[0]))).columns
+        return [dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader(f'Inversor {i + 1}'),
+                    dbc.CardBody([
+                        dcc.Dropdown(
+                            id=f'drop-inv-{i}',
+                            placeholder='Coluna respectiva',
+                            options=[
+                                {'label': coluna, 'value': coluna}
+                                for coluna in df_colunas],
+                        ),
+                    ])
+                ], color='dark')
+            ], sm=4)
+            for i in range(int(n))
+        ]),
+        ]
+    except Exception as e:
+        print(e)
+        return html.P('Tabela não encontrada')
+
+
+csv = pd.read_excel(r'app\templates\formularios\temp.xlsx')
+
+
+@cad_banco_1.callback(
+    Output('tabs-cols-db', 'active_tab'),
+    Input('red-cm', 'n_clicks'),
+    Input('ipt-n-invs', 'value'),
+    Input('ipt-tbl-base', 'value'),
+    [Input(f'drop-inv-{i}', 'value') for i in range(int(csv['quantidade_de_inversores']))]
+)
+def redireciona_para_cm(*args):
+    df3 = pd.read_excel(r'app\templates\formularios\temp.xlsx')
+    if dash.ctx.triggered_id == 'red-cm' and args[1] and args[2] and args[3:]:
+        for i in range(3, int(df3['quantidade_de_inversores'])):
+            df3['colunas_dos_inversores'] = args[i]
+        return 'tab-cm'
+
+@cad_banco_1.callback(
+    Output('row-cm', 'children'),
+    Input('check-cm', 'value')
+)
+def mostra_opcoes_cm(tem):
+    if tem:
+        return dbc.Col([
+            dbc.Row([
+                dbc.Checkbox(id='check-irrad-h', label='Possui Irradiação Solar Horizontal?', value=False)
+            ]),
+            dbc.Row([
+                dbc.Checkbox(id='check-irrad-i', label='Possui Irradiação Solar Inclinada?', value=False)
+            ]),
+            dbc.Row([
+                dbc.Checkbox(id='check-temp-amb', label='Possui Temperatura Ambiente?', value=False)
+            ]),
+            dbc.Row([
+                dbc.Checkbox(id='check-temp-plc', label='Possui Temperatura das Placas?', value=False)
+            ]),
+            dbc.Row([
+                dbc.Checkbox(id='check-umd-rel-ar', label='Possui Umidade Relativa do Ar?', value=False)
+            ]),
+        ], sm=12)
